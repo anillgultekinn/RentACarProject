6526418bbc5416cfff9da57c323e48b3caee8fa8
Anıl Gültekin <117902946+anillgultekinn@users.noreply.github.com>
2024-03-25 02:14:48 +0300

    Add project files.

---
+++ src/rentACarProject/Domain/Entities/Model.cs
@@ -0,0 +1,16 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Model : Entity<Guid>
+{
+    public Guid BrandId { get; set; }
+    public Guid FuelId { get; set; }
+    public string Name { get; set; }
+    public decimal DailyPrice { get; set; }
+    public string ImageUrl { get; set; }
+
+    public virtual Brand? Brand { get; set; }
+    public virtual Fuel? Fuel { get; set; }
+
+}
